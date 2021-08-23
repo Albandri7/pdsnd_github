c5bc58db79ac8ddbf7071b6b5cc3a9690d871246
albandri <sagry_a@hotmail.com>
2021-08-23 10:14:25 +0300

    capitalize months names

--- Desktop/thired-project/.bikeshare.py
+++ Desktop/thired-project/.bikeshare.py
@@ -29,7 +29,7 @@ def get_filters():
     while True:    
         month = input("Do you want to display information for a particular month? If yes, enter month name from within first six months else enter 'all'")
         month = month.lower()
-        if month in ['january', 'february', 'march', 'april', 'may', 'june', 'all']:
+        if month in ['January', 'February', 'March', 'April', 'May', 'June', 'all']:
             break
         else:
             print("invalid input.try again")
